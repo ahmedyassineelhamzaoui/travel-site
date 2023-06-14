5949dcabd76760b19bf9670d054210b39a663137
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-06-14 19:55:04 +0100

    add allnecessary layouts for dashboard

--- resources/views/admin/dashboard/users.blade.php
+++ resources/views/admin/dashboard/users.blade.php
@@ -1,1 +1,41 @@
-users
+﻿@extends('layouts.dashboard-app')
+
+@section('title', 'users')
+
+@section('content')
+
+    <div id="preloader">
+        <div class="sk-three-bounce">
+            <div class="sk-child sk-bounce1"></div>
+            <div class="sk-child sk-bounce2"></div>
+            <div class="sk-child sk-bounce3"></div>
+        </div>
+    </div>
+    
+    <div id="main-wrapper">
+
+        
+		
+		@include('layouts.dashboard-header')
+
+        @include('layouts.dashboard-sidebar')
+		
+		
+        <div class="content-body">
+			<div class="container-fluid">
+				<div class=" form-head d-flex flex-wrap mb-4 align-items-center">
+					<h2 class="text-black mr-auto font-w600 mb-3">Users</h2>
+
+				</div>
+			
+            </div>
+		</div>
+    </div>
+	
+        
+
+        @include('layouts.dashboard-footer')
+
+
+  
+@endsection
---
+++ resources/views/layouts/dashboard-footer.blade.php
@@ -0,0 +1,5 @@
+<div class="footer">
+    <div class="copyright">
+        <p>Copyright © Designed &amp; Developed by <a href="../index.html" target="_blank">Ahmed Yassine El hamzaoui</a> 2023</p>
+    </div>
+</div>
